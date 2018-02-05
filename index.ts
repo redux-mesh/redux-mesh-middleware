c32defab878344d874a430b2d2f6fb34486b91ce
el-davo <david.ahern26@gmail.com>
2018-02-05 20:33:42 +0000

    Added options

--- index.ts
+++ index.ts
@@ -1,6 +1,27 @@
-import {Action, Middleware, Store} from 'redux';
+import {Middleware, Store} from 'redux';
+import {IOptions} from './lib/IOptions';
+import {utils} from './lib/utils';
 
-export const reduxMesh: Middleware = (store: Store<any>) => (next) => (action) => {
-    console.log(action);
-    return next(action);
+export const reduxMesh = (options: IOptions): Middleware => {
+
+    const sessionId = utils.generateSessionId();
+
+    const mesh: Middleware = (store: Store<any>) => (next) => (action) => {
+
+        const timestamp = new Date().getTime();
+        const eventType = action.type;
+        const event = action;
+
+        const body = {
+            timestamp,
+            sessionId,
+            eventType,
+            event,
+            store: store.getState()
+        };
+
+        return next(action);
+    };
+
+    return mesh;
 };
