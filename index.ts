74ed4678f7b177fb6023820f209b5a6b39c6e738
el-davo <david.ahern26@gmail.com>
2018-02-08 20:18:15 +0000

    Added requesting session

--- index.ts
+++ index.ts
@@ -1,22 +1,31 @@
 import {Middleware, Store} from 'redux';
 import {IOptions} from './lib/IOptions';
-import {utils} from './lib/utils';
 import {IEvent} from './lib/IEvent';
 import {request} from './lib/request';
+import {ISession} from './lib/ISession';
 
 export const reduxMesh = (options: IOptions): Middleware => {
 
-    const sessionId = utils.generateSessionId();
-    const url = options.url ? options.url : 'http://localhost:3000/api/events';
+    const url = options.url ? options.url : 'http://localhost:3000';
+    let session: ISession;
+
+    try {
+        (async () => {
+            session = await request.createSession(url, options.token);
+        })();
+    } catch (ex) {
+        console.error(ex);
+    }
 
     const mesh: Middleware = (store: Store<any>) => (next) => (action) => {
-
         const timestamp: number = new Date().getTime();
         const eventType: string = action.type;
 
-        const event: IEvent = {timestamp, sessionId, eventType, event: action, store: store.getState()};
+        const event: IEvent = {timestamp, sessionId: session.id, eventType, event: action, store: store.getState()};
 
-        request.post(url, options.token, event);
+        if (session) {
+            request.sendEvent(url, options.token, event);
+        }
 
         return next(action);
     };
